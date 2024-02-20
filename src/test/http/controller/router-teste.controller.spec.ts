38dac4f0741fe2e29a5c8e3075f5eecf837dce34
kleyson Castro <kleysoncastro99@gmail.com>
2024-02-20 16:23:24 -0300

    test: valida roda de test

---
+++ src/test/http/controller/router-teste.controller.spec.ts
@@ -0,0 +1,27 @@
+import request from "supertest";
+import { app } from "@/app";
+
+import { it, describe, expect, beforeAll, afterAll } from "vitest";
+
+describe("Router test (e2e)", async () => {
+	beforeAll(async () => {
+		await app.ready();
+	});
+
+	afterAll(async () => {
+		await app.close();
+	});
+
+	it("Espera-se que seja possivel receber um json ok da roda /test", async () => {
+		const response = await request(app.server).get("/teste");
+
+
+		console.log(response.body);
+		expect(response.statusCode).toEqual(200);
+		expect(response.body).toEqual(expect.objectContaining({
+			ok: true
+		}));
+	});
+
+
+});
